f2b8a0e49cc9cea68f1997d8282f4c7d8d4cc875
Theo Paris <theoparisdesigns@gmail.com>
2021-07-07 16:33:44 -0700

    fix: updated logger

--- src/logger.ts
+++ src/logger.ts
@@ -1,34 +1,13 @@
-import { BuildResult, Plugin, PluginBuild } from "esbuild";
 import chalk from "chalk";
-import ora from "ora";
 
+// TODO: @firenodes/core logger
 type Logger = (message: string | Error) => void;
 
-function createLogger(prefix: string): Logger {
-    return (message) => {
+const createLogger =
+    (prefix: string): Logger =>
+    (message) => {
         console.log(`${prefix} ${message}`);
     };
-}
-
-const buildSpinner = ora({
-    color: "magenta"
-});
-
-export const buildSpinnerPlugin: Plugin = {
-    name: "buildSpinner",
-    setup(build: PluginBuild): void {
-        build.onStart(() => {
-            buildSpinner.start("Rebuilding...");
-        });
-        build.onEnd((result: BuildResult) => {
-            if (result.errors.length > 0) {
-                buildSpinner.clear();
-            } else {
-                buildSpinner.succeed("Built!");
-            }
-        });
-    }
-};
 
 export default {
     info: createLogger(chalk.bgHex("#3d33e5").bold("info")),
--- src/plugins/logger/index.ts
+++ src/plugins/logger/index.ts
@@ -1,15 +1,5 @@
 import { BuildResult, Plugin, PluginBuild } from "esbuild";
-import chalk from "chalk";
 import ora from "ora";
-
-// TODO: @firenodes/core logger
-type Logger = (message: string | Error) => void;
-
-const createLogger =
-    (prefix: string): Logger =>
-    (message) => {
-        console.log(`${prefix} ${message}`);
-    };
 
 const buildSpinner = ora({
     color: "magenta"
@@ -29,10 +19,4 @@ export const buildSpinnerPlugin: Plugin = {
             }
         });
     }
-};
-
-export default {
-    info: createLogger(chalk.bgHex("#3d33e5").bold("info")),
-    warn: createLogger(chalk.bgYellow.bold("warn")),
-    error: createLogger(chalk.bgRedBright.bold("error"))
 };
